36f909c4edf6bf2d39c176bb1ec535e84ac8fc19
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-04-04 09:05:51 -0400

    adding menu

--- todoApp.py
+++ todoApp.py
@@ -1,1 +1,26 @@
-print("Testing")
+
+todoList = []
+
+def addTask():
+  print("add")
+
+
+def editTask():
+  print("edit")
+
+
+def removedTask():
+  print("Removed")
+
+
+menu ="""
++-----------------------+
+  Todo List 📋 | Menu
++-----------------------+
+1) add
+2) edit
+3) removed
+Option type the Number for :
+"""
+
+print(menu)
