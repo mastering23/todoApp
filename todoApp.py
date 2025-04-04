742b70cd365d901c0071526aeb094c3a29f2069c
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-04-04 10:42:52 -0400

    adding menu

--- todoApp.py
+++ todoApp.py
@@ -1,5 +1,17 @@
-
+op =""
+task = ""
 todoList = []
+
+menu ="""
++-----------------------+
+  Todo List 📋 | Menu
++-----------------------+
+1) add
+2) edit
+3) removed
+00) Exit
+Option type the Number for
+"""
 
 def addTask():
   print("add")
@@ -13,14 +25,20 @@ def removedTask():
   print("Removed")
 
 
-menu ="""
-+-----------------------+
-  Todo List 📋 | Menu
-+-----------------------+
-1) add
-2) edit
-3) removed
-Option type the Number for :
-"""
+def Menu():
+  while True:
+    print(menu)
+    op = int(input(":"))
+    if op == 1:
+        addTask()
+    elif op == 2:
+        editTask()
+    elif op == 3:
+        removedTask()
+    elif op == 00 :
+      print("exiting")
+      break
+    else:
+      print("Invalid option, please try again")
 
-print(menu)
+Menu()
